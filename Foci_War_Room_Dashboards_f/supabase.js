18e0f33cb223edb24aec133251a24084a4ea64ec
Thabo Ntentesa <onthatilententesa@gmail.com>
2026-06-23 10:23:43 +0200

    Upadated supabase.js

--- Foci_War_Room_Dashboards_f/supabase.js
+++ Foci_War_Room_Dashboards_f/supabase.js
@@ -4,3 +4,12 @@ const SUPABASE_URL = 'https://blqhhrumgsblakzxjuaa.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscWhocnVtZ3NibGFrenhqdWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MTU2NDIsImV4cCI6MjA5NjQ5MTY0Mn0.9GGT_au6CqkV3oeQXI4TO8iYzx24_qqEfSS5WFtssVM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+const uploadWebFile = async (file) => {
+  const { data, error } = await supabase.storage
+    .from('website-uploads')
+    .upload(`public/${file.name}`, file)
+
+  if (error) console.error('Upload failed:', error)
+  return data
+}
